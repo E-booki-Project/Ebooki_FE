dbc26da1060360dfba579636579ba8310319a097
jiwonee02 <nepthun@naver.com>
2025-10-30 08:56:50 +0900

    feat: 소셜 로그인 아이콘 이미지 추가

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import * as F from "../../styles/auth/ForgotPasswordStyle";
 
 function ForgotPassword() {
-    return (
-        <F.ForgotPassword>
-            <F.PasswordContainer>
-                <F.Form>
-                    <F.InputWrapper>
-                        <F.InputLabel>이메일</F.InputLabel>
-                        <F.InputFeild placeholder="booki@example.com" />
-                    </F.InputWrapper>
-                    <F.InputWrapper>
-                        <F.InputLabel>새 비밀번호</F.InputLabel>
-                        <F.InputFeild placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
-                    </F.InputWrapper>
-                    <F.InputWrapper>
-                        <F.InputLabel>비밀번호 확인</F.InputLabel>
-                        <F.InputFeild placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
-                    </F.InputWrapper>
-                    <F.PasswordButton>비밀번호 변경하기</F.PasswordButton>
-                </F.Form>
-            </F.PasswordContainer>
-        </F.ForgotPassword>
-    );
+    return <div>ForgotPassword</div>;
 }
 
 export default ForgotPassword;
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,37 +1,7 @@
 import React from "react";
-import * as S from "../../styles/auth/SignupStyle";
 
-import plus from "../../assets/images/plus.png";
-
-function signup() {
-    return (
-        <S.Signup>
-            <S.SignupContainer>
-                <S.Form>
-                    <S.Profile>
-                        <S.ProfileIcon src={plus} />
-                    </S.Profile>
-                    <S.InputWrapper>
-                        <S.InputLabel>아이디</S.InputLabel>
-                        <S.InputFeild placeholder="booki" />
-                    </S.InputWrapper>
-                    <S.InputWrapper>
-                        <S.InputLabel>이메일</S.InputLabel>
-                        <S.InputFeild placeholder="booki@example.com" />
-                    </S.InputWrapper>
-                    <S.InputWrapper>
-                        <S.InputLabel>비밀번호</S.InputLabel>
-                        <S.InputFeild placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
-                    </S.InputWrapper>
-                    <S.InputWrapper>
-                        <S.InputLabel>비밀번호 확인</S.InputLabel>
-                        <S.InputFeild placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
-                    </S.InputWrapper>
-                    <S.SignupButton>가입하기</S.SignupButton>
-                </S.Form>
-            </S.SignupContainer>
-        </S.Signup>
-    );
+function Signup() {
+    return <div>Signup</div>;
 }
 
-export default signup;
+export default Signup;
