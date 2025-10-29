95e53825143986b7ff73f2b60278010fcc2b5d0b
jiwonee02 <nepthun@naver.com>
2025-10-29 18:21:13 +0900

    start: 1차 라우터 세팅

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Signin from "./pages/auth/Signin";
+import Signup from "./pages/auth/Signup";
+import Detail from "./pages/books/Detail";
+import List from "./pages/books/List";
+import Reader from "./pages/books/Reader";
+import Invite from "./pages/team/Invite";
+import Join from "./pages/team/Join";
+import Pricing from "./pages/pricing/Pricing";
+
+function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/forgotpassword" element={<ForgotPassword />} />
+                <Route path="/books" element={<List />} />
+                <Route path="/reader/:bookId" element={<Reader />} />
+                <Route path="/books/detail/:bookId" element={<Detail />} />
+                <Route path="/invite/:teamId" element={<Invite />} />
+                <Route path="/join/:teamId" element={<Join />} />
+                <Route path="/pricing" element={<Pricing />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    font-family: "Pretendard", "NanumMyeongjo", sans-serif;
+    overflow-x: hidden;
+    min-height: 100vh;
+    background-color: #F2F2F2;
+    color: #1E1E1E;
+  }
+
+  @supports (-webkit-touch-callout: none) {
+    body {
+      min-height: -webkit-fill-available;
+    }
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
