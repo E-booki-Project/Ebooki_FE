358e17eb6bc4e5770fc26aede9d86fdd21031aff
jiwonee02 <nepthun@naver.com>
2025-10-30 09:49:20 +0900

    feat: 색상 팔레트 및 폰트 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+    blue: "#89D1D9",
+    pink: "#F2B6D7",
+    yellow: "#F2CF66",
+    green: "#CDF2A7",
+    coral: "#F2949C",
+    white: "#FFFFFF",
+
+    black: "#1E1E1E",
+    grayDark: "#757575",
+    grayLight: "#B3B3B3",
+    grayLighter: "#F5F5F5",
+};
--- src/styles/typography.js
+++ src/styles/typography.js
@@ -17,4 +17,22 @@ export const typography = {
         line-height: 140%;
         font-weight: 400;
     `,
+
+    bodyStrong: css`
+        font-size: 16px;
+        line-height: 140%;
+        font-weight: 700;
+    `,
+
+    bodySmall: css`
+        font-size: 14px;
+        line-height: 140%;
+        font-weight: 400;
+    `,
+
+    subHeading: css`
+        font-size: 20px;
+        line-height: 120%;
+        font-weight: 400;
+    `,
 };
