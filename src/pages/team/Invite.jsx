82638d0b6d640d56dcbea189891dfa2a12bec57f
jiwonee02 <nepthun@naver.com>
2025-10-30 10:28:08 +0900

    feat: 팀 초대 페이지 구조 설계

--- src/pages/team/Invite.jsx
+++ src/pages/team/Invite.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import * as I from "../../styles/team/InviteStyle";
 
 function Invite() {
-    return <div>Invite</div>;
+    return (
+        <I.Invite>
+            <I.InviteContainer>
+                <I.InviteHeader>
+                    <I.BackIcon />
+                </I.InviteHeader>
+                <I.BookWrapper>
+                    <I.Book />
+                    <I.BookDesc>가을, 김유정</I.BookDesc>
+                </I.BookWrapper>
+                <I.InputWrapeer>
+                    <I.InputFeild placeholder="팀 이름을 입력해주세요" />
+                    <I.BackIcon />
+                </I.InputWrapeer>
+                <I.LinkContainer>
+                    <I.LinkWrapper>
+                        <I.LinkIcon />
+                        <I.LinkTitle>Shareable Link is now ready!</I.LinkTitle>
+                        <I.LinkSub>
+                            링크를 생성하고 친구랑 같이 읽어 보세요.
+                        </I.LinkSub>
+                    </I.LinkWrapper>
+                    <I.LinkButton>Copy Link</I.LinkButton>
+                </I.LinkContainer>
+                <I.ListContainer>
+                    <I.ListWrapper>
+                        <I.Profile />
+                        <I.Username>user name</I.Username>
+                    </I.ListWrapper>
+                    <I.More />
+                </I.ListContainer>
+            </I.InviteContainer>
+        </I.Invite>
+    );
 }
 
 export default Invite;
---
+++ src/styles/team/InviteStyle.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { typography } from "../typography";
+import { colors } from "../colors";
+
+export const Invite = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`;
+
+export const InviteContainer = styled.div`
+    width: 596px;
+    background-color: ${colors.white};
+    border: 1px solid ${colors.grayMedium};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 24px;
+    border-radius: 8px;
+`;
+
+export const InviteHeader = styled.div``;
+
+export const BackIcon = styled.img``;
+
+export const BookWrapper = styled.div``;
+
+export const Book = styled.img``;
+
+export const BookDesc = styled.div``;
+
+export const InputWrapeer = styled.div``;
+
+export const InputFeild = styled.input``;
+
+export const LinkContainer = styled.div``;
+
+export const LinkWrapper = styled.div``;
+
+export const LinkIcon = styled.img``;
+
+export const LinkTitle = styled.div``;
+
+export const LinkSub = styled.div``;
+
+export const LinkButton = styled.button``;
+
+export const ListContainer = styled.div``;
+
+export const ListWrapper = styled.div``;
+
+export const Profile = styled.img``;
+
+export const Username = styled.div``;
+
+export const More = styled.img``;
