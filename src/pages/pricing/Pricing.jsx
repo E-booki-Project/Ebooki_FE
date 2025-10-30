ad5bd0dde7f5f2840cf6e00111e21b30f8ca1458
jiwonee02 <nepthun@naver.com>
2025-10-30 14:14:50 +0900

    feat: 요금제 페이지 기본 구조 설계

--- src/pages/pricing/Pricing.jsx
+++ src/pages/pricing/Pricing.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import * as P from "../../styles/pricing/PricingStyle";
 
 function Pricing() {
-    return <div>Pricing</div>;
+    return (
+        <P.Pricing>
+            <P.TitleContainer>
+                <P.Title>요금제 선택</P.Title>
+                <P.SubTitle>
+                    같이 책을 읽을 수 있는 횟수를 판매해요
+                    <br />
+                    읽는 즐거움에, 나누는 감동을 더하세요
+                </P.SubTitle>
+            </P.TitleContainer>
+            <P.PlanContainer>
+                <P.PlanCard>
+                    <P.PlanHeader>
+                        <P.PlanIcon />
+                        <P.PlanWrapper>
+                            <P.PlanName>요금제 이름</P.PlanName>
+                            <P.PlanPrice>
+                                9,900<span> / 2+1 권</span>
+                            </P.PlanPrice>
+                        </P.PlanWrapper>
+                    </P.PlanHeader>
+                    <P.PlanContent>
+                        <P.PlanDesc></P.PlanDesc>
+                        <P.PlanDesc></P.PlanDesc>
+                        <P.PlanDesc></P.PlanDesc>
+                    </P.PlanContent>
+                    <P.PlanButton>요금제 선택</P.PlanButton>
+                </P.PlanCard>
+            </P.PlanContainer>
+        </P.Pricing>
+    );
 }
 
 export default Pricing;
---
+++ src/styles/pricing/PricingStyle.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { typography } from "../typography";
+import { colors } from "../colors";
+
+export const Pricing = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`;
+
+export const TitleContainer = styled.div``;
+
+export const Title = styled.div``;
+
+export const SubTitle = styled.div``;
+
+export const PlanContainer = styled.div``;
+
+export const PlanCard = styled.div``;
+
+export const PlanHeader = styled.div``;
+
+export const PlanIcon = styled.img``;
+
+export const PlanWrapper = styled.div``;
+
+export const PlanName = styled.div``;
+
+export const PlanPrice = styled.div``;
+
+export const PlanContent = styled.div``;
+
+export const PlanDesc = styled.div``;
+
+export const PlanButton = styled.button``;
