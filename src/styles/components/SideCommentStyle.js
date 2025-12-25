e3ee21d6987a97ba3a4a7e0a93033f5a4d928815
jiwonee02 <nepthun@naver.com>
2025-12-25 20:10:00 +0900

    feat: 사이드 댓글 구조 구성

---
+++ src/styles/components/SideCommentStyle.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+// import { typography } from "../typography";
+// import { colors } from "../colors";
+
+export const SideComment = styled.div``;
+
+export const Tape = styled.div``;
+
+export const BookQuotes = styled.div``;
+
+export const CommentContainer = styled.div``;
+
+export const CommentHeader = styled.div``;
+
+export const Profile = styled.div``;
+
+export const ProfileIcon = styled.img``;
+
+export const ProfileName = styled.div``;
+
+export const MoreIcon = styled.img``;
+
+export const CommentContent = styled.div``;
+
+export const CommentReaction = styled.div``;
+
+export const ReactionIcon = styled.img``;
+
+export const ReactionCount = styled.div``;
+
+export const InputWrapper = styled.div``;
+
+export const Input = styled.input``;
+
+export const EnterIcon = styled.img``;
