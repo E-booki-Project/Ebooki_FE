43fd332180cb2c38c8320d92259364b4716facbb
jiwonee02 <nepthun@naver.com>
2025-12-26 20:53:23 +0900

    feat: 헤더 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import * as H from "../styles/components/HeaderStyle";
+import logo from "../assets/images/logo.png";
 
-function Header() {
-    return <div>Header</div>;
+function AppHeader({ isLoggedIn = false, onLogout }) {
+    const navigate = useNavigate();
+
+    const handleAuthClick = () => {
+        if (isLoggedIn) {
+            if (onLogout) onLogout();
+            else navigate("/");
+        } else {
+            navigate("/signin");
+        }
+    };
+
+    return (
+        <H.Header>
+            <H.Logo src={logo} alt="logo" onClick={() => navigate("/")} />
+
+            <H.MenuWrapper>
+                <H.MenuNav>
+                    <H.MenuLink to="/pricing" end>
+                        요금제
+                    </H.MenuLink>
+                    <H.MenuLink to="/books">도서 리스트</H.MenuLink>
+                    <H.MenuLink to="/teams">팀</H.MenuLink>
+                </H.MenuNav>
+
+                <H.LoginBtn as="button" type="button" onClick={handleAuthClick}>
+                    {isLoggedIn ? "로그아웃" : "로그인"}
+                </H.LoginBtn>
+            </H.MenuWrapper>
+        </H.Header>
+    );
 }
 
-export default Header;
+export default AppHeader;
