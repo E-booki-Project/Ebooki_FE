33782e6fb8126c68da42ab8a779de2eb3f966e6f
jiwonee02 <nepthun@naver.com>
2026-01-08 19:55:59 +0900

    feat: 팀리스트 페이지 구조 잡기

---
+++ src/styles/team/TeamListStyle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+// import { typography } from "../typography";
+// import { colors } from "../colors";
+
+export const TeamList = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`;
