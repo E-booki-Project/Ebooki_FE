e706800f169257a88710491fb9055045d971c075
jiwonee02 <nepthun@naver.com>
2026-01-24 14:02:48 +0900

    feat: 프로필 수정 페이지 구현

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import * as M from "../../styles/mypage/MypageStyle";
+import ProfileView from "./ProfileView";
+import ProfileEdit from "./ProfileEdit";
 
 function Mypage() {
-    return <div>Mypage</div>;
+    return (
+        <M.Mypage>
+            <M.Layout>
+                <M.Section>
+                    <ProfileEdit />
+                </M.Section>
+                <M.Section></M.Section>
+            </M.Layout>
+        </M.Mypage>
+    );
 }
 
 export default Mypage;
---
+++ src/styles/mypage/ProfileEditStyle.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import { typography } from "../typography";
+import { colors } from "../colors";
+
+export const ProfileEdit = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 36px;
+    width: 100%;
+    height: 100%;
+`;
+
+export const ProfileWrapper = styled.button`
+    width: 170px;
+    height: 170px;
+    border-radius: 50%;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    margin-bottom: 28px;
+`;
+
+export const ProfileIcon = styled.img`
+    position: absolute;
+    inset: 0;
+    margin: auto;
+    width: 60px;
+    height: 60px;
+    z-index: 4;
+    pointer-events: none;
+`;
+
+export const ProfileInput = styled.input`
+    display: none;
+`;
+
+export const ProfileImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+
+export const AccountSettings = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: ${colors.white};
+    border: 1px solid ${colors.grayMedium};
+    padding: 16px 23px;
+`;
+
+export const Form = styled.form`
+    gap: 16px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`;
+
+export const InputLabel = styled.label`
+    ${typography.bodyBase};
+`;
+
+export const InputFeild = styled.input`
+    width: 404px;
+    height: 40px;
+    border: 1px solid ${colors.grayMedium};
+    border-radius: 8px;
+    padding: 12px 16px;
+    &::placeholder {
+        color: ${colors.grayLight};
+        ${typography.bodyBaseSingle};
+    }
+`;
+
+export const EditButton = styled.button`
+    width: 407px;
+    height: 32px;
+    background-color: ${colors.grayLight};
+    border: none;
+    border-radius: 8px;
+    color: ${colors.black};
+    ${typography.bodyBaseSingle};
+    text-align: center;
+    margin-top: 8px;
+`;
+
+export const WithdrawButton = styled.button`
+    margin-top: 32px;
+    border: none;
+    background-color: transparent;
+    color: #c00f0c;
+    ${typography.bodySmall};
+    text-decoration: underline;
+`;
