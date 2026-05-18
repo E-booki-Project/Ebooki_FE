7bbd02f0b8548e68e1c6f8b13720dc41ca2f2689
yeon-yeon1 <nojk0106@naver.com>
2026-05-19 01:55:57 +0900

    ✨ Feat: 토큰 재발급 연동
    Feat: 토큰 재발급 연동

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import { getAccessToken } from "../utils/authStorage";
+import { getAccessToken, getRefreshToken, getUserInfo, setAuthStorage, clearAuthStorage } from "../utils/authStorage";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
@@ -12,14 +14,75 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = getAccessToken();
-
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
-
         return config;
     },
     (error) => Promise.reject(error),
+);
+
+let isRefreshing = false;
+let pendingQueue = [];
+
+const processQueue = (error, token = null) => {
+    pendingQueue.forEach(({ resolve, reject }) =>
+        error ? reject(error) : resolve(token)
+    );
+    pendingQueue = [];
+};
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status !== 401 || originalRequest._retry) {
+            return Promise.reject(error);
+        }
+
+        if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+                pendingQueue.push({ resolve, reject });
+            }).then((token) => {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return axiosInstance(originalRequest);
+            });
+        }
+
+        originalRequest._retry = true;
+        isRefreshing = true;
+
+        try {
+            const accessToken = getAccessToken();
+            const refreshToken = getRefreshToken();
+
+            const { data } = await axios.post(`${BASE_URL}/auth/reissue`, {
+                accessToken,
+                refreshToken,
+            });
+
+            const newAccessToken = data.data.accessToken;
+            const newRefreshToken = data.data.refreshToken ?? refreshToken;
+
+            setAuthStorage({
+                accessToken: newAccessToken,
+                refreshToken: newRefreshToken,
+                userInfo: getUserInfo(),
+            });
+
+            processQueue(null, newAccessToken);
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            return axiosInstance(originalRequest);
+        } catch (reissueError) {
+            processQueue(reissueError, null);
+            clearAuthStorage();
+            window.location.replace("/signin");
+            return Promise.reject(reissueError);
+        } finally {
+            isRefreshing = false;
+        }
+    },
 );
 
 export default axiosInstance;
